ec126085e0b2d3c68036438c019cb9032683d7a8
Carolina S. M <carostmaria@gmail.com>
2021-11-13 18:28:34 -0300

    Compose index to render the wallet status data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-export default function Home() {
+import Balance from '../components/Balance/Balance'
+import Actions from '../components/Actions/Actions'
+import Transactions from '../components/Transactions/Transactions'
+
+import styles from '../styles/MyWallet.module.css'
+
+function MyWallet() {
   return (
     <div>
-     
+      <h1>Mi cuenta</h1>
+      <Balance />
+      <Actions />
+      <Transactions />
     </div>
   )
 }
+
+export default MyWallet
