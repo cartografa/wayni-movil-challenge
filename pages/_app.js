3bd5a6c0b2c1f49c2a4b4b2e9c879319af9770df
Carolina S. M <carostmaria@gmail.com>
2021-11-13 18:25:11 -0300

    Make TabBar visible for all 'pages'

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import TabBar from '../components/TabBar/TabBar'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <TabBar />
+    </>
+  )
 }
 
 export default MyApp
