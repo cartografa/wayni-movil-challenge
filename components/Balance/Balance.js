eae4dcf30736b5ba6d843e01ee3368d4a84a1b38
Carolina S. M <carostmaria@gmail.com>
2021-11-14 16:20:10 -0300

    Work on implementing copy to clipboard function and number normalization

--- components/Balance/Balance.js
+++ components/Balance/Balance.js
@@ -1,31 +1,42 @@
+import { useRef } from 'react'
 import useUser from '../../hooks/useUser'
 
 import styles from './Balance.module.css'
 
+
 function Balance() {
+  const cvuRef = useRef()
   const { data, isLoading, isError } = useUser()
 
   if (isLoading) return 'Cargando...'
   if (isError) return 'Error :('
   
-  // console.log(data)
-  const balance = data.result.balance.result.total
+  // convierto el saldo con separadores de punto y coma.
+  const balance = data.result.balance.result.total.toLocaleString('de-DE')
 
-  if (!isLoading && !isError) {
-    return (
-      <div className={styles.card}>
-        <h3 className={styles.title}>SALDO DISPONIBLE</h3>
-        <h2 className={styles.balance}>{`$ ${balance}`}</h2>
-        <hr className={styles.horizontal_line}/>
-        <div className={styles.cvu_container}>
-          <p>CVU: <b>{data.result.bankInfo.result.cvu}</b></p>
-          <button>Copiar</button>
-        </div>
-      </div>
-    )
+  const cvu = data.result.bankInfo.result.cvu
+
+  // copio cvu
+  const handleCopyToClipboard = (text) => {
+    navigator.clipboard.writeText(text).then(() => {
+      console.log('Copiado! :)')
+    }, (err) => {
+      console.error('No se pudo copiar', err)
+    })
   }
-
-
+ 
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.title}>SALDO DISPONIBLE</h3>
+      <h2 className={styles.balance}>{`$ ${balance}`}</h2>
+      <hr className={styles.horizontal_line}/>
+      <div className={styles.cvu_container}>
+        <p>CVU: <strong ref={cvuRef}>{cvu}</strong></p>
+        <button onClick={() => handleCopyToClipboard(cvuRef.current.innerText)}>Copiar</button>
+      </div>
+    </div>
+  )
 }
+
 
 export default Balance
