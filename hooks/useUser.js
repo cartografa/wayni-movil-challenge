a38b333b8a4189891d2a4ec7909d31235f43933f
Carolina S. M <carostmaria@gmail.com>
2021-11-16 18:17:07 -0300

    Minor fixes

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -1,15 +1,11 @@
 import useSWR from 'swr'
 
 const url = `https://run.mocky.io/v3/d3586928-f1a4-48d2-9a0e-1140bab1e562`
-
-// creo el fetcher para swr
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
-function useUser() {
-  // obtengo la data y el status
-  const { data, error } = useSWR(url, fetcher)
+function useUser(...args) {
+  const { data, error } = useSWR(url, fetcher, ...args)
   
-  // retorno objeto con data y status
   return {
     data: data,
     isLoading: !error && !data,
